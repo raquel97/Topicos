adf80c757f612f5a94e31a9ad35818e6a9aff4e1
Laboratorio <Laboratorio@SP-LA-LAB9-15>
2020-02-15 15:00:11 -0600

    Solucion inicial

---
+++ NorthWind/NorthWind/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NorthWind.Startup))]
+namespace NorthWind
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
