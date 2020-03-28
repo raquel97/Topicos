400f73339e762b846081b86ddc32e9b48d775fff
raquel <raquel-valverde@outlook.com>
2020-03-27 20:12:58 -0600

    mvc

---
+++ NorthWind/MVC/Controllers/HolaController.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class HolaController
+        Inherits Controller
+
+        ' GET: Hola
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Hola/Details/5
+        Function Details(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Hola/Create
+        Function Create() As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Hola/Create
+        <HttpPost()>
+        Function Create(ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add insert logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Hola/Edit/5
+        Function Edit(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Hola/Edit/5
+        <HttpPost()>
+        Function Edit(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add update logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Hola/Delete/5
+        Function Delete(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Hola/Delete/5
+        <HttpPost()>
+        Function Delete(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add delete logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+    End Class
+End Namespace
