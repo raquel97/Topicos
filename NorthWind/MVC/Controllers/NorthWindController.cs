b0c3080d2e992d3b5c109524121b7c3e20b64a03
raquel <raquel-valverde@outlook.com>
2020-04-18 10:08:22 -0600

    MVC

--- NorthWind/MVC/Controllers/NorthWindController.cs
+++ NorthWind/MVC/Controllers/NorthWindController.cs
@@ -4,12 +4,93 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace MVC.Controllers
 {
     public class NorthWindController : Controller
     {
-        // GET: NorthWind
+        NorthwindEntities contexto = new NorthwindEntities();
+        // GET: NorthWindMVC
         public ActionResult Index()
+        {
+
+            List<Models.Northwind> lista = (from pro in contexto.Products
+                                            join cat in contexto.Categories on pro.CategoryID equals cat.CategoryID
+                                            join sup in contexto.Suppliers on pro.SupplierID equals sup.SupplierID
+                                            select new Models.Northwind
+                                            {
+                                                ProductID = pro.ProductID,
+                                                ProductName = pro.ProductName,
+                                                CategoryName = cat.CategoryName,
+                                                UnitPrice = pro.UnitPrice,
+                                                UnitsInStock = pro.UnitsInStock,
+                                                CompanyName = sup.CompanyName,
+                                                Country = sup.Country,
+                                                PostalCode = sup.PostalCode,
+                                                Address = sup.Address,
+                                                Phone = sup.Phone
+
+                                            }).ToList();
+
+            return View(lista);
+
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Models.Northwind model)
+        {
+            if (ModelState.IsValid)
+            {
+                Products productos = new Products();
+                Suppliers proveedores = new Suppliers();
+                Categories categorias = new Categories();
+                productos.ProductID = model.ProductID;
+                productos.ProductName = model.ProductName;
+                categorias.CategoryName = model.CategoryName;
+                productos.UnitPrice = model.UnitPrice;
+                productos.UnitsInStock = model.UnitsInStock;
+                proveedores.CompanyName = model.CompanyName;
+                proveedores.Country = model.Country;
+                proveedores.PostalCode = model.PostalCode;
+                proveedores.Address = model.Address;
+                proveedores.Phone = model.Phone;
+
+
+
+                contexto.Products.Add(productos);
+                contexto.Suppliers.Add(proveedores);
+                contexto.Categories.Add(categorias);
+                contexto.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+            return View(model);
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            Products producto = (from p in contexto.Products
+                                 where p.ProductID == id
+                                 select p).FirstOrDefault();
+            contexto.Products.Remove(producto);
+            contexto.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Modificar(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Modificar(Models.Northwind model)
         {
             return View();
         }
---
+++ NorthWind/MVC/Models/Northwind.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Northwind
+    {
+        public int ProductID { get; set; }
+
+        [Display(Name = "Producto")]
+        [Required(ErrorMessage = "Debe ingresar el nombre del producto")]
+        //[StringLength(15, ErrorMessage = "El nombre no debe exceder los")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Categoría")]
+        [Required(ErrorMessage = "Debe ingresar la categoría del producto")]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "Debe ingresar el precio del producto")]
+        public decimal? UnitPrice { get; set; }
+
+        [Display(Name = "Stock")]
+        [Required(ErrorMessage = "Debe ingresar el stock del producto")]
+        public short? UnitsInStock { get; set; }
+
+        [Display(Name = "Compañía")]
+        [Required(ErrorMessage = "Debe ingresar la compañía del producto")]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "Debe ingresar el país del producto")]
+        public string Country { get; set; }
+
+        [Display(Name = "Código Postal")]
+        [Required(ErrorMessage = "Debe ingresar el código postal")]
+        public string PostalCode { get; set; }
+
+        [Display(Name = "Dirección")]
+        [Required(ErrorMessage = "Debe ingresar la dirección de la compañía")]
+        public string Address { get; set; }
+
+        [Display(Name = "Teléfono")]
+        [Required(ErrorMessage = "Debe ingresar el teléfono de la compañía")]
+        public string Phone { get; set; }
+    }
+}
