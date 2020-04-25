4aa52c3a314b5f88093b062fde166b5fd7ec44c2
raquel <raquel-valverde@outlook.com>
2020-04-24 22:33:34 -0600

    MVC

--- NorthWind/MVC/Models/CategoriesNorthWind.cs
+++ NorthWind/MVC/Models/CategoriesNorthWind.cs
@@ -7,5 +7,14 @@ namespace MVC.Models
 {
     public class CategoriesNorthWind
     {
+        public int CategoryID { get; set; }
+
+        [Display(Name = "Categoria")]
+        [Required(ErrorMessage = "Debe ingresar la categoria")]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "Descripción")]
+        [Required(ErrorMessage = "Debe ingresar la descripción")]
+        public string Description { get; set; }
     }
 }
